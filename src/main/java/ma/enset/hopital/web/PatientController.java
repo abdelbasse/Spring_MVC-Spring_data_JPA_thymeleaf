3ce72adf97b76d283bfabddbbbfd966c76e58a7c
abdelbasse <taoudiabdu@gmail.com>
2025-04-14 01:57:33 +0100

    Add pagination

--- src/main/java/ma/enset/hopital/web/PatientController.java
+++ src/main/java/ma/enset/hopital/web/PatientController.java
@@ -1,5 +1,8 @@
 package ma.enset.hopital.web;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.ui.Model;
 import lombok.AllArgsConstructor;
 import ma.enset.hopital.model.Patient;
@@ -7,6 +10,7 @@ import ma.enset.hopital.repo.PatientRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,11 +21,24 @@ public class PatientController {
     private PatientRepo patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model){
-        List<Patient> patientList = patientRepository.findAll();
-        model.addAttribute("ListPatients",patientList);
+    public String index(Model model,
+                        @RequestParam(name = "page" , defaultValue = "0") int page,
+                        @RequestParam(name = "size",defaultValue = "5") int size){
+        Page<Patient> pagePatientList = patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("ListPatients",pagePatientList.getContent());
+        model.addAttribute("pages",new int[pagePatientList.getTotalPages()]);
+        model.addAttribute("currentPage",page);
         return "patients";
     }
+    //    public String index(Model model, HttpServletRequest request)
+//    {
+//        int page = Integer.parseInt(request.getParameter("page"));
+//        int size = Integer.parseInt(request.getParameter("size"));
+//        Page<Patient> pagePatientList = patientRepository.findAll(PageRequest.of(page,size));
+//        model.addAttribute("ListPatients",pagePatientList.getContent());
+//        model.addAttribute("pages",new int[pagePatientList.getTotalPages()]);
+//        return "patients";
+//    }
 
     @GetMapping("/")
     public String home(Model model){
